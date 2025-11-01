c3449ea8949317dce4b34de6e00616e32c23af7d
Gabriel <kasekaki120@gmail.com>
2025-10-31 21:24:09 -0300

    aula de javascript1

---
+++ trocaimg.js
@@ -0,0 +1,15 @@
+function mostracheio(imagem){
+    imagem.src="carrinho_cheio.png";
+}
+
+function mostravazio(imagem){
+    imagem.src="carrinho_vazio.png";
+}
+
+function carrim(imagem){
+    imagem.src="carrim.png";
+}
+
+function carrim2(imagem){
+    imagem.src="carrim2.png";
+}
