e3460d6bf1b69f103fa4e12c184cfe8cb3feeaea
kasekaki120 <kasekaki120@gmail.com>
2025-11-14 20:54:45 -0300

    Add files via upload

---
+++ aula.php
@@ -0,0 +1,7 @@
+<?php 
+$variavel="Gabriel";
+$numero=16;
+
+echo " Hello World  welcome "  . $variavel . " seu numero da sorte " . $numero
+
+?>
---
+++ trataformulario.php
@@ -0,0 +1,6 @@
+<?php
+
+echo "O produto " . $_POST['produto']. " tem " .$_POST['quantidade'] . "quantidade em estoque " . "com o preço de " . $_POST['preco']. " cor " .$_POST['cor'];
+
+
+?>
